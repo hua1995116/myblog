8be289b4156a47ae5361babee2dc8abb1b487786
hua1995116 <461249104@qq.com>
2020-01-20 17:25:39 +0800

    fix: desc

---
+++ article-init.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const template = fs.readFileSync('./template/template.md').toString();
+
+const date = new Date();
+
+const year = date.getFullYear();
+
+const month = date.getMonth();
+
+const day = date.getDate();
+
+fs.writeFileSync(`./notes/${year}-${(month+1) > 10 ? (month + 1) : '0' + month }-${day}.md`, template);
