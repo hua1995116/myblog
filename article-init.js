5f12eff08ad02b5b28bbb73d6eb9b56b2f6ee33e
hua1995116 <461249104@qq.com>
2020-11-14 22:55:00 +0800

    feat: article

--- article-init.js
+++ article-init.js
@@ -10,4 +10,4 @@ const month = date.getMonth();
 
 const day = date.getDate();
 
-fs.writeFileSync(`./notes/${year}-${(month+1) > 10 ? (month + 1) : '0' + month }-${day > 10 ? day : '0' + day}.md`, template);
+fs.writeFileSync(`./notes/${year}-${(month+1) > 10 ? (month + 1) : '0' + (month+1) }-${day > 10 ? day : '0' + day}.md`, template);
